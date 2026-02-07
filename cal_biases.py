6fb9ae35c535fbf01529a851c9460c052950c766
Shurui Lin <wenyinli@gh-login02.delta.ncsa.illinois.edu>
2026-02-07 15:55:41 -0600

    Calibration with magnitude cut

--- cal_biases.py
+++ cal_biases.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 import galsim
 import torch
 from tqdm import tqdm
-from src.anacal.cal_toolkit import load_img, build_dataset
-from src.anacal.batch_calibration import prepare_q_images, Calibrator, get_biases
-from src.architecture.single_gal_CNN import R180Inv_CNN_GeLU, D4T_CNN_GeLU,SmoothCNN_GeLU
-from src.architecture.forward8_CNN import Forward8_fixW_CNN, SmoothCNN_GeLU_mocked,Forward8_fixW_nores_CNN, Forward8_attnW_CNN
-import argparse, os, random
+from src.anacal.cal_toolkit import selection_response
+from src.anacal.batch_calibration import get_biases
+import argparse, os, random, re
 import time
 
 
@@ -31,6 +29,38 @@ def format_number(x: float) -> str:
     s = s.replace('.', '')                  # remove the decimal point
     return f"n{s}"
 
+def parse_info(fname, folder, cal_result):
+    # ==== parse noise ====
+    # folder = "n0594"
+    def parse_noise_level(folder):
+        s = folder[1:]
+        if len(s) == 4:
+            return float(s) / 1000.0
+        elif len(s) == 3:
+            return float(s) / 100.0
+        elif len(s) == 2:
+            return float(s) / 10.0
+        elif len(s) == 1:
+            return float(s)
+        else:
+            raise ValueError(f"Unexpected noise level format: {folder}")
+
+    noise_level = parse_noise_level(folder)
+
+    
+    # ==== parse psf_fwhm ====
+    # fname = "p85_f8_2e7_m253"
+    # 取 p85 -> 8.5 / 10 = 0.85
+    psf_match = re.search(r"p(\d+)", fname)
+    psf_fwhm = float(psf_match.group(1)) / 100.0 if psf_match else None
+
+    # ==== extract values ====
+    m, c, m_err, c_err = cal_result
+
+    # ==== output string ====
+    out = f"{noise_level},{psf_fwhm},{fname},{m.tolist()},{m_err.tolist()},{c.tolist()},{c_err.tolist()}"
+    return out
+
 def get_calibration_biases(
         grid_per_img = 100,     # imgs per npy file
         imgBs = 100,            # batch size for npy file
@@ -38,15 +68,20 @@ def get_calibration_biases(
         folder_name = 'n0',
         fname = 'p85',
         is_twin = True,
-        workers = 32,
+        workers = 16,
+        mag_cut = None,
+        flux_name = 'fpfs_flux',
         ):
-
+    zero_point = 30.0
     img_nB = img_nB_range[1]-img_nB_range[0]
     Nimg = imgBs*(img_nB_range[1]-img_nB_range[0])*grid_per_img
     print(Nimg)
 
-    g_all = np.zeros([grid_per_img*imgBs*img_nB, 4, 2])
+    g_all = np.zeros([grid_per_img*imgBs*img_nB, 5, 2])
     R_all = np.zeros([grid_per_img*imgBs*img_nB, 4, 2, 2])
+    if mag_cut is not None:
+        flux_all = np.zeros([grid_per_img*imgBs*img_nB,5,1])
+        Rflux_all = np.zeros([grid_per_img*imgBs*img_nB,1,2])
     shear_tasks = [
                 (1, "g1"),
                 (0, "g1"),
@@ -66,34 +101,54 @@ def get_calibration_biases(
             end = (index+1)*imgBs*grid_per_img
             g_all[begin:end,i] = np.load(f'{dir_gr}{shear_comp}_{shear_mode}/{fname}_shapes_{index}.npy')
             R_all[begin:end,i] = np.load(f'{dir_gr}{shear_comp}_{shear_mode}/{fname}_Rana_{index}.npy')[:,0]
+            if mag_cut is not None:
+                flux_all[begin:end,i] = np.load(f'{dir_gr}{shear_comp}_{shear_mode}/{flux_name}_m00_{index}.npy')
 
-    #dir = '/work/hdd/bdsp/wenyinli/datasets/xlens_gal/'
-    all_cutouts, all_psfs, all_cats, noise_level = build_dataset(
-            shear_comp='g1', shear_mode=0, abs_shear_value=0.02,
-            noise_level=0, 
-            index_range=[0,imgBs*(img_nB_range[1]-img_nB_range[0])], 
-            directory=dir, 
-            workers=workers, 
-            return_imgs=False,
-            use_threads=False
+
+    if mag_cut is not None:
+        for index in range(img_nB_range[0], img_nB_range[1]):
+            begin = index*imgBs*grid_per_img
+            end = (index+1)*imgBs*grid_per_img
+            flux_all[begin:end,4] = np.load(f'{dir_gr}g1_2/{flux_name}_m00_{index}.npy')
+            g_all[begin:end,4] = np.load(f'{dir_gr}g1_2/{fname}_shapes_{index}.npy') 
+            Rflux_all[begin:end,0] = np.load(f'{dir_gr}g1_2/{flux_name}_Rm00_{index}.npy') # [ngal, 1, 2]
+        R_sel = selection_response(
+            flux=flux_all[:,4,0],
+            R_flux=Rflux_all[:,0],
+            shapes=g_all[:,4,:],
+            mag_cut=mag_cut,
         )
+        print(f"Selection response for flux cut at mag {mag_cut}: {R_sel}")
+        R_all[:,:,1,1] += R_sel[1]
+        R_all[:,:,0,0] += R_sel[0]
+        mask = zero_point - 2.5 * np.log10(flux_all[:,:4,0]) < mag_cut
+        print(f"Number of galaxies after mag cut at {mag_cut}: {np.sum(mask)/4}")
 
-    cal_result = get_biases(g_all, R_all, 
+
+
+    cal_result = get_biases(g_all[:,:4], R_all, mask=mask,
                                     bs_size=grid_per_img*imgBs*(img_nB_range[1]-img_nB_range[0]), 
                                     bs_times=100,
                                     n_factor=1,
-                                    is_twin=True,
-                                    n_jobs=16,
+                                    is_twin=is_twin,
+                                    n_jobs=workers,
+                                    chunk_bs=20,
                                     )
     #print(cal_result)
-    print(f'm={cal_result[0]} +- {cal_result[2]}, c={cal_result[1]} +- {cal_result[3]}')
+    
+    print(parse_info(fname, folder_name, cal_result))
 
     save_name = f'/work/hdd/bfmo/wenyinli/datasets/xlens_cali_results/{folder_name}/'
     if not os.path.exists(save_name):
         os.makedirs(save_name)
     np.save(save_name+f'{fname}_g_all.npy', g_all)
     np.save(save_name+f'{fname}_R_all.npy', R_all)
-    all_cats.to_csv(save_name+f'{fname}_cats_all.csv')
+    #all_cats.to_csv(save_name+f'{fname}_cats_all.csv')
+    with open("./logs/calibration_result.csv","a") as f:
+        if mag_cut is not None:
+            f.write(parse_info(fname+'_mag'+str(mag_cut), folder_name, cal_result) + "\n")
+        else:
+            f.write(parse_info(fname, folder_name, cal_result) + "\n")
 
 
 if __name__ == "__main__":
@@ -102,22 +157,27 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--noise", type=float, required=True, help="Noise level for calibration")
     p.add_argument("--fname", type=str, default="test", help="Filename prefix")
+    p.add_argument("--mag_cut", type=float, default=None, help="Magnitude cut for selection")
     args = p.parse_args()
 
     noise_level = args.noise
     fname = args.fname
-
+    mag_cut = args.mag_cut
     print(f"Starting shape measurement and calibration for noise level: {noise_level}")
     print(f"Using filename prefix: {fname}")
-
+    if mag_cut is not None:
+        print(f"Applying magnitude cut at: {mag_cut}")
 
     get_calibration_biases(
         grid_per_img=100,
         imgBs=100,
-        img_nB_range= [0,2000],
+        img_nB_range= [0,4000],
         folder_name=format_number(noise_level),
         is_twin=True,
         fname=fname,
+        workers=64,
+        mag_cut=mag_cut,
+        flux_name='fpfs_mag',
     )
 
     end_time = time.time()
--- shape_measurement.py
+++ shape_measurement.py
@@ -7,23 +7,21 @@ from tqdm import tqdm
 from src.anacal.cal_toolkit import load_img, build_dataset
 from src.anacal.batch_calibration import prepare_q_images, Calibrator, get_biases
 from src.architecture.single_gal_CNN import R180Inv_CNN_GeLU, D4T_CNN_GeLU,SmoothCNN_GeLU
-from src.architecture.forward8_CNN import Forward8_fixW_CNN, SmoothCNN_GeLU_mocked,Forward8_fixW_nores_CNN, Forward8_attnW_CNN
-import argparse, os, random
+from src.architecture.forward8_CNN import Forward8_fixW_CNN, SmoothCNN_GeLU_mocked,Forward8_fixW_nores_CNN, Forward8_simp_CNN
+import argparse, os, random,re
 import time
 
 
 device = ("cuda" if torch.cuda.is_available() else "cpu")
-#model_path = './models/F8_TBigUni_l5c32r0_5ep.pth'
-model_path = './models/forward8_CNN_nada_50ep.pth'
-#model = D4T_CNN_GeLU(num_layers = 8).to(device)
-model = Forward8_fixW_CNN(num_layers = 5, base_channels = 32, res_factor=0.1).to(device)
-#model = Forward8_attnW_CNN(num_layers = 5, base_channels = 64, res_factor=1).to(device)
+#model_path = './models/forward8_CNN_nada_50ep.pth'
+model_path = './models/F8simp_TBigUni_l5c32r0_20ep.pth'
+#model = Forward8_fixW_CNN(num_layers = 5, base_channels = 32, res_factor=0.1).to(device)
+model = Forward8_simp_CNN(num_layers = 5, base_channels = 32, res_factor=0).to(device)
+
 model.load_state_dict(torch.load(model_path, map_location="cpu"))
-#model.load_state_dict(torch.load('./models/avg_CNN_n0.1_50ep.pth', map_location="cpu"))
 _ = model.eval()
-cal_CNN = Calibrator(model, device)
+#dir = '/work/nvme/bfmo/wenyinli/datasets/xlens_pe005'
 dir = '/work/nvme/bfmo/wenyinli/datasets/xlens_shift'
-#dir = '/work/hdd/bdsp/wenyinli/datasets/xlens_fixed'
 
 shear_tasks = [
             (0, "g1"),
@@ -48,7 +46,9 @@ def loop_shape_measurement(
     imgBs = 100,            # batch size for npy file
     img_nB_range = [0,100],           # number of npy batch
     noise_level = 0,
-    fname = 'p85'    
+    fname = 'p85',
+    dtype = 'float32',
+    psf_fwhm = 0.85,    
     ):
     if noise_level == 'adaptive':
         folder_name = 'nada'
@@ -58,6 +58,7 @@ def loop_shape_measurement(
     print(f"Using filename prefix: {fname}")
     print(f"Selected shear tasks: {shear_tasks}")
     print(f"Dataset directory: {dir}")
+
     for shear_mode, shear_comp in shear_tasks:
         for start in range(img_nB_range[0], img_nB_range[1]):
             print(f'Processing {shear_comp}_{shear_mode}, batch {start}')
@@ -67,77 +68,27 @@ def loop_shape_measurement(
                     noise_level=noise_level, index_range=range_idx, directory=dir, workers=32, use_threads=False
                 )
             
-            np.random.seed(20020520+start)
+            np.random.seed(20020620+start)
             noises = np.random.normal(0, 1, all_cutouts.shape)*noise_levels.reshape(-1,1,1)
             q_imgs = prepare_q_images(
                     all_cutouts, all_psfs, noises, all_cats,
-                    sigma_arcsec=0.85/2.355,
+                    sigma_arcsec=psf_fwhm/2.355,
                     workers=32, flim=10
                 )
             q_imgs = q_imgs[:,:,1:,1:]
+
+            if dtype == 'float32':
+                q_imgs = q_imgs.astype(np.float32)
+            elif dtype == 'float64':
+                q_imgs = q_imgs.astype(np.float64)
             
-            shapes, R_ana = calibrator.shape_measure(q_imgs,batch_size=700)
+            shapes, R_ana = calibrator.shape_measure(q_imgs,batch_size=800)
             save_name = f'/projects/bfmo/wenyinli/datasets/xlens_sims/{folder_name}/{shear_comp}_{shear_mode}/'
             if not os.path.exists(save_name):
                 os.makedirs(save_name)
             np.save(save_name + f'{fname}_shapes_{start}.npy', shapes)
             np.save(save_name + f'{fname}_Rana_{start}.npy', R_ana)
             del all_cutouts, all_psfs, all_cats, noises, q_imgs, shapes, R_ana
-
-def get_calibration_biases(
-        grid_per_img = 100,     # imgs per npy file
-        imgBs = 100,            # batch size for npy file
-        img_nB = 100,           # number of npy batch
-        folder_name = 'n0',
-        fname = 'p85',
-        workers = 32,
-        ):
-
-    Nimg = imgBs*img_nB*grid_per_img
-    print(Nimg)
-
-    g_all = np.zeros([grid_per_img*imgBs*img_nB, 4, 2])
-    R_all = np.zeros([grid_per_img*imgBs*img_nB, 4, 2, 2])
-    shear_tasks = [
-                (1, "g1"),
-                (0, "g1"),
-                (1, "g2"),
-                (0, "g2"),
-            ]
-    dir_gr = f'/projects/bdsp/wenyinli/datasets/xlens_sims/{folder_name}/'
-    for i, (shear_mode, shear_comp) in enumerate(shear_tasks):
-        for index in range(img_nB):
-            begin = index*imgBs*grid_per_img
-            end = (index+1)*imgBs*grid_per_img
-            g_all[begin:end,i] = np.load(f'{dir_gr}{shear_comp}_{shear_mode}/{fname}_shapes_{index}.npy')
-            R_all[begin:end,i] = np.load(f'{dir_gr}{shear_comp}_{shear_mode}/{fname}_Rana_{index}.npy')[:,0]
-
-    #dir = '/work/hdd/bdsp/wenyinli/datasets/xlens_gal/'
-    all_cutouts, all_psfs, all_cats, noise_level = build_dataset(
-            shear_comp='g1', shear_mode=2, abs_shear_value=0.02,
-            noise_level=0, 
-            index_range=[0,imgBs*img_nB], 
-            directory=dir, 
-            workers=workers, 
-            return_imgs=False,
-            use_threads=False
-        )
-
-    cal_result = get_biases(g_all, R_all, 
-                                    bs_size=grid_per_img*imgBs*img_nB, 
-                                    bs_times=100,
-                                    n_factor=1,
-                                    n_jobs=16,
-                                    )
-    #print(cal_result)
-    print(f'm={cal_result[0]} +- {cal_result[2]}, c={cal_result[1]} +- {cal_result[3]}')
-
-    save_name = f'/work/nvme/bfmo/wenyinli/datasets/xlens_sims/{folder_name}/'
-    if not os.path.exists(save_name):
-        os.makedirs(save_name)
-    np.save(save_name+f'{fname}_g_all.npy', g_all)
-    np.save(save_name+f'{fname}_R_all.npy', R_all)
-    all_cats.to_csv(save_name+f'{fname}_cats_all.csv')
 
 
 
@@ -170,18 +121,20 @@ if __name__ == "__main__":
     noise_level = args.noise
     fname = args.fname
     shear_tasks = parse_shear_tasks(args.shear_tasks)
-
+    dtype = 'float32'
 
     print(f"Path of the model dict: {model_path}")
-
+    cal_CNN = Calibrator(model, device, dtype=dtype)
     # --- Run main processes ---
     loop_shape_measurement(
         shear_tasks=shear_tasks,
         calibrator=cal_CNN,
         imgBs=100,          # batch size per npy
-        img_nB_range=[1400,2000],         # number of npy batches
+        img_nB_range=[2000,4000],         # number of npy batches
         noise_level=noise_level,
+        psf_fwhm=0.85,
         fname=fname,
+        dtype=dtype,
     )
 
     end_time = time.time()
